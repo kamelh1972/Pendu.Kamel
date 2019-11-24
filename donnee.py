5dd8e3a4a126f3a01ccab2a6e516df62ea81a1ae
hammiche <kamelhammiche44@gmail.com>
2019-11-24 20:28:23 +0100

    add declarations des variables dans donnees.py

---
+++ donnee.py
@@ -0,0 +1,4 @@
+nombre_essai = 8
+mon_fichier_scores = "scores"
+liste_mots=["ardoise","argenterie","bateau","biberon","chapeau","croquette","dromadaire","éléphanteaux","hiboux",
+"journal","librairie","ouragan","robinet","rustique","souris","tabouret","ventilateur","zèbres"]
