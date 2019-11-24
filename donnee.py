140b18fca41bd492d81c7a62cb94453d2540cd7d
hammiche <kamelhammiche44@gmail.com>
2019-11-24 20:55:18 +0100

    add modif donnee.py

--- donnee.py
+++ donnee.py
@@ -1,4 +1,4 @@
-nombre_essai = 8
-mon_fichier_scores = "scores"
+chance = 8
+
 liste_mots=["ardoise","argenterie","bateau","biberon","chapeau","croquette","dromadaire","éléphanteaux","hiboux",
 "journal","librairie","ouragan","robinet","rustique","souris","tabouret","ventilateur","zèbres"]
